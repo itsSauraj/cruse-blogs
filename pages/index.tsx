d4fb8d393ad3ff3eeb492014a116282890bc29d9
Saurabh Yadav <sauraj.contact@gmail.com>
2024-12-24 19:28:25 +0530

    project skeleton

---
+++ pages/index.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@nextui-org/button";
+import { Code } from "@nextui-org/code";
+import Link from "next/link";
+import { Snippet } from "@nextui-org/snippet";
+import { title } from "@/components/primitives";
+
+import DefaultLayout from "@/layouts/default";
+
+
+export default function IndexPage() {
+  return (
+    <DefaultLayout>
+      <div className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <img
+          alt="sauraj"
+          className="w-[250px] h-[250px] rounded-full"
+          loading="lazy"
+          src={`https://www.creativefabrica.com/wp-content/uploads/2022/11/21/Black-Boy-Retro-Charming-Avatar-47769583-1.png`}
+        />
+        <h3 className={`${title({ color: "blue" })} font-bolder`}>
+          Hi, I am Sauraj
+        </h3>
+        <Snippet hideCopyButton hideSymbol variant="bordered">
+          <span>
+            I write blogs on{" "}
+            <Code color="primary">fullstack web application development</Code>
+          </span>
+        </Snippet>
+        <Button
+          as={Link}
+          color="primary"
+          href="/blogs"
+          size="lg"
+          variant="ghost"
+        >
+          Read my blogs
+        </Button>
+      </div>
+    </DefaultLayout>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const { nextui } =  require('@nextui-org/theme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx, mdx}', 
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './layouts/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        mono: ["var(--font-mono)"],
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+}
+
